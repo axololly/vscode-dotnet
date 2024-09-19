ad9f6b6d81ae39dae0ad131e2cc57fd262b7acbe
Axololly <jamesgriggs362@gmail.com>
2024-09-19 21:16:44 +0100

    First commit - edited dotnet.ts to run in release

--- src/dotnet.ts
+++ src/dotnet.ts
@@ -7,14 +7,14 @@ import { Executor } from "./executor";
 export class Dotnet {
     public static build(fileUri: vscode.Uri) {
         if (fileUri && fileUri.fsPath) {
-            Executor.runInTerminal(`dotnet build "${fileUri.fsPath}"`);
+            Executor.runInTerminal(`dotnet build -c release "${fileUri.fsPath}"`);
             AppInsightsClient.sendEvent("build", { fileExtension: path.extname(fileUri.fsPath) });
         }
     }
 
     public static run(fileUri: vscode.Uri) {
         if (fileUri && fileUri.fsPath) {
-            Executor.runInTerminal(`dotnet run --project "${fileUri.fsPath}"`);
+            Executor.runInTerminal(`dotnet run -c release --project "${fileUri.fsPath}"`);
             AppInsightsClient.sendEvent("run", { fileExtension: path.extname(fileUri.fsPath) });
         }
     }
